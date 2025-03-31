51a7c8e7ac83c9addd24939b25b1c29f55de1fec
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-31 13:29:45 +0200

    Initial commit

---
+++ global.d.ts
@@ -0,0 +1,18 @@
+declare global {
+  interface ElementInternals {
+    states: Pick<
+      Set<string>,
+      | 'size'
+      | 'add'
+      | 'clear'
+      | 'delete'
+      | 'entries'
+      | 'forEach'
+      | 'has'
+      | 'keys'
+      | 'values'
+    >;
+  }
+}
+
+export {};
---
+++ whitespace-tracking-gdpr.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Plugin Name: Whitespace Tracking & GDPR
+ * Description: Adds tracking for Matomo and cookie consent for GDPR compliance.
+ * Version: 0.0.0
+ * Author: Whitespace Dev
+ * Text Domain: whitespace-tracking-gdpr
+ * Domain Path: /languages/
+ */
+
+define("WHITESPACE_TRACKING_GDPR_PLUGIN_FILE", __FILE__);
+define("WHITESPACE_TRACKING_GDPR_PATH", dirname(__FILE__));
+define("WHITESPACE_TRACKING_GDPR_URL", rtrim(plugin_dir_url(__FILE__), "/"));
+define(
+  "WHITESPACE_TRACKING_GDPR_AUTOLOAD_PATH",
+  WHITESPACE_TRACKING_GDPR_PATH . "/autoload",
+);
+define(
+  "WHITESPACE_TRACKING_GDPR_LANGUAGES_PATH",
+  plugin_basename(dirname(__FILE__)) . "/languages",
+);
+
+add_action("plugins_loaded", function () {
+  load_plugin_textdomain(
+    "whitespace-tracking-gdpr",
+    false,
+    WHITESPACE_TRACKING_GDPR_LANGUAGES_PATH,
+  );
+});
+
+add_action("muplugins_loaded", function () {
+  load_muplugin_textdomain(
+    "whitespace-tracking-gdpr",
+    WHITESPACE_TRACKING_GDPR_LANGUAGES_PATH,
+  );
+});
+
+array_map(static function () {
+  include_once func_get_args()[0];
+}, glob(WHITESPACE_TRACKING_GDPR_AUTOLOAD_PATH . "/*.php"));
