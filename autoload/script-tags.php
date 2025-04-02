38d70f36c520fe5e19ee516bdc1dd38580c4528c
Fredrik Johansson <jeanfredrik@gmail.com>
2025-04-02 14:01:41 +0200

    fix: Missing type on module scripts

--- autoload/script-tags.php
+++ autoload/script-tags.php
@@ -17,13 +17,16 @@ add_filter(
     // error_log(var_export($attributes["src"], true));
     if (strpos($attributes["src"], get_site_url() . "/") !== 0) {
       $attributes["data-category"] = "uncategorized";
+      if (isset($attributes["type"])) {
+        $attributes["data-type"] = $attributes["type"];
+      }
       $attributes["type"] = "text/plain";
     }
     // if ($attributes["id"] != "whitespace-tracking-gdpr-js") {
     //   $attributes["data-category"] = "uncategorized";
     //   $attributes["type"] = "text/plain";
     // }
-    error_log(var_export($attributes, true));
+    // error_log(var_export($attributes, true));
     return $attributes;
   },
   10,
