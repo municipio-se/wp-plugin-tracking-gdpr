b4baf88187ebb0023ad8e4c7618155dc0bd10a86
Fredrik Johansson <jeanfredrik@gmail.com>
2025-05-02 11:52:24 +0200

    Add "wstg_script_service" filter

--- autoload/script-tags.php
+++ autoload/script-tags.php
@@ -17,9 +17,35 @@ add_filter(
       strpos($attributes["src"], get_site_url() . "/") === 0
         ? ""
         : "uncategorized";
+
+    /**
+     * Filter the category of the script
+     *
+     * @hook wstg_script_category
+     * @since 0.0.0
+     *
+     * @param string $category The category of the script
+     * @return string The category of the script
+     */
     $category = apply_filters("wstg_script_category", $category);
+
     if ($category) {
       $attributes["data-category"] = $category;
+
+      /**
+       * Filter the service of the script
+       *
+       * @hook wstg_script_service
+       * @since 0.0.0
+       *
+       * @param string $service The service of the script
+       * @return string The service of the script
+       */
+      $service = apply_filters("wstg_script_service", "");
+      if ($service) {
+        $attributes["data-service"] = $service;
+      }
+
       if (isset($attributes["type"])) {
         $attributes["data-type"] = $attributes["type"];
       }
