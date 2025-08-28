f095d118d66d38dbd2c80d27062a1d0924f9245d
Fredrik Johansson <jeanfredrik@gmail.com>
2025-08-28 21:36:41 +0200

    Format

--- autoload/cookie-consent.php
+++ autoload/cookie-consent.php
@@ -352,51 +352,52 @@ add_action("admin_head-nav-menus.php", function () {
     __("Tracking and GDPR"),
     function () {
       ?>
-            <div id="posttype-cookie" class="posttypediv">
-                <div id="tabs-panel-cookie" class="tabs-panel tabs-panel-active">
-                    <ul id="cookie-checklist" class="categorychecklist form-no-clear">
-                        <li>
-                            <label class="menu-item-title">
-                                <input type="checkbox" class="menu-item-checkbox"
-                                       name="menu-item[-1][menu-item-object-id]" value="-1">
-                                <?php _e(
-                                  "Change cookie preferences",
-                                  "whitespace-tracking-gdpr",
-                                ); ?>
-                            </label>
-
-                            <input type="hidden" name="menu-item[-1][menu-item-type]" value="custom">
-                            <input type="hidden" name="menu-item[-1][menu-item-title]" value="<?php esc_attr_e(
-                              "Change cookie preferences",
-                              "whitespace-tracking-gdpr",
-                            ); ?>">
-                            <input type="hidden" name="menu-item[-1][menu-item-url]" value="#">
-                            <input type="hidden" name="menu-item[-1][menu-item-classes]" value="wstg-trigger-cookie-dialog">
-                        </li>
-                    </ul>
-                </div>
-
-                <?php wp_nonce_field(
-                  "add-menu_item",
-                  "menu-settings-column-nonce",
+      <div id="posttype-cookie" class="posttypediv">
+        <div id="tabs-panel-cookie" class="tabs-panel tabs-panel-active">
+          <ul id="cookie-checklist" class="categorychecklist form-no-clear">
+            <li>
+              <label class="menu-item-title">
+                <input
+                  type="checkbox"
+                  class="menu-item-checkbox"
+                  name="menu-item[-1][menu-item-object-id]"
+                  value="-1"
+                >
+                <?php _e(
+                  "Change cookie preferences",
+                  "whitespace-tracking-gdpr",
                 ); ?>
-                <p class="button-controls wp-clearfix">
-                    <span class="list-controls">
-                        <a href="#" class="select-all"><?php _e(
-                          "Select All",
-                        ); ?></a>
-                    </span>
-                    <span class="add-to-menu">
-                        <input type="submit"
-                               class="button submit-add-to-menu right"
-                               id="submit-posttype-cookie"
-                               name="add-post-type-menu-item"
-                               value="<?php esc_attr_e("Add to Menu"); ?>">
-                        <span class="spinner"></span>
-                    </span>
-                </p>
-            </div>
-            <?php
+              </label>
+
+              <input type="hidden" name="menu-item[-1][menu-item-type]" value="custom">
+              <input type="hidden" name="menu-item[-1][menu-item-title]" value="<?php esc_attr_e(
+                "Change cookie preferences",
+                "whitespace-tracking-gdpr",
+              ); ?>">
+              <input type="hidden" name="menu-item[-1][menu-item-url]" value="#">
+              <input type="hidden" name="menu-item[-1][menu-item-classes]" value="wstg-trigger-cookie-dialog">
+            </li>
+          </ul>
+        </div>
+
+        <?php wp_nonce_field("add-menu_item", "menu-settings-column-nonce"); ?>
+        <p class="button-controls wp-clearfix">
+          <span class="list-controls">
+            <a href="#" class="select-all"><?php _e("Select All"); ?></a>
+          </span>
+          <span class="add-to-menu">
+            <input
+              type="submit"
+              class="button submit-add-to-menu right"
+              id="submit-posttype-cookie"
+              name="add-post-type-menu-item"
+              value="<?php esc_attr_e("Add to Menu"); ?>"
+            >
+            <span class="spinner"></span>
+          </span>
+        </p>
+      </div>
+      <?php
     },
     "nav-menus",
     "side",
