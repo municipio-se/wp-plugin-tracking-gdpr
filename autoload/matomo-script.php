0ea048b81b65fdf0216308c80413d4b17b67c32d
jeanfredrik <jeanfredrik@gmail.com>
2025-10-05 00:07:25 +0200

    Fix: Cookies were stored when Matomo was run in container mode

--- autoload/matomo-script.php
+++ autoload/matomo-script.php
@@ -12,23 +12,24 @@ add_action("wp_head", function () {
   $url = mx_get_matomo_option("url");
   $container_id = mx_get_matomo_option("container_id");
   $site_id = mx_get_matomo_option("site_id");
-  if ($container_id && $url): ?>
-      <!-- Matomo Tag Manager -->
-      <script<?php echo wp_sanitize_script_attributes(
-        apply_filters("wp_inline_script_attributes", []),
-      ); ?>>
-        var _mtm = window._mtm = window._mtm || [];
-        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-        (function() {
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true;
-          g.src='<?php echo $url; ?>js/container_' + '<?php echo $container_id; ?>' + '.js';
-          s.parentNode.insertBefore(g,s);
-        })();
-      </script>
-      <!-- End Matomo Tag Manager -->
-      <?php elseif (!$container_id && $url && $site_id): ?>
-        <!-- Matomo -->
+  if($url && ($container_id || $site_id)):
+    if($container_id):
+      ?>
+        <script<?php echo wp_sanitize_script_attributes(
+          apply_filters("wp_inline_script_attributes", []),
+        ); ?>>
+          var _mtm = window._mtm = window._mtm || [];
+          _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+          (function() {
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true;
+            g.src='<?php echo $url; ?>js/container_' + '<?php echo $container_id; ?>' + '.js';
+            s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+      <?php
+    elseif($site_id):
+      ?>
         <script<?php echo wp_sanitize_script_attributes(
           apply_filters("wp_inline_script_attributes", []),
         ); ?>>
@@ -44,20 +45,23 @@ add_action("wp_head", function () {
             g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
           })();
         </script>
-        <script<?php echo wp_sanitize_script_attributes(
-          apply_filters("wp_inline_script_attributes", []),
-        ); ?> data-category="analytics" type="text/plain">
-          _paq.push(['rememberCookieConsentGiven']);
-          console.log('rememberCookieConsentGiven');
-        </script>
-        <script<?php echo wp_sanitize_script_attributes(
-          apply_filters("wp_inline_script_attributes", []),
-        ); ?> data-category="!analytics" type="text/plain">
-          _paq.push(['forgetCookieConsentGiven']);
-          console.log('forgetCookieConsentGiven');
-        </script>
-        <!-- End Matomo Code -->
-        <?php endif;
+      <?php
+    endif;
+    ?>
+      <script<?php echo wp_sanitize_script_attributes(
+        apply_filters("wp_inline_script_attributes", []),
+      ); ?> data-category="analytics" type="text/plain">
+        _paq.push(['rememberCookieConsentGiven']);
+        console.log('rememberCookieConsentGiven');
+      </script>
+      <script<?php echo wp_sanitize_script_attributes(
+        apply_filters("wp_inline_script_attributes", []),
+      ); ?> data-category="!analytics" type="text/plain">
+        _paq.push(['forgetCookieConsentGiven']);
+        console.log('forgetCookieConsentGiven');
+      </script>
+    <?php
+  endif;
 });
 
 add_action("init", function () {
